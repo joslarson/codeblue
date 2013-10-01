a790378999deab6c520158cab2ab2a649ab87054
Joseph Larson <larson.joseph@gmail.com>
2013-10-01 14:19:51 -0600

    Add final click!

--- js/page.js
+++ js/page.js
@@ -4,33 +4,19 @@ $(function(){
     port.onMessage.addListener(function(msg) {
         if (msg.options['status'] == "on"){
             var filename = window.location.href.substr( window.location.href.lastIndexOf("/") + 1 );
-            // if(filename == "Login.asp" || filename == "login.asp"){
-            //     $('#UserID').val(setOpt(msg.options['username'], ""));
-            //     $('#Password').val(setOpt(msg.options['password'], ""));
-            //     if($('#UserID').val() !== "" && $('#Password').val() !== ""){
-            //         $('#submit1').click();
-            //     }
-            // }
-            // if(filename == "Home.asp"){
-            //     window.location.replace("http://jetblue.rp4me.com/Employee_FormatEntry_Home.asp");
-            // }
-
-            // if(filename == "Employee_FormatEntry_Home.asp"){
-            //     var location = $('a:contains(Advance VTO)').attr('href');
-            //     window.location.replace(location);
-            // }
+            
             if(filename == "Employee_FormatEntry1.asp?FormatID=96&EmpID=32957&Admin=&FormatName=Advance%20VTO"){
                 $("select[name='ExceptionDate'] :nth-child(3)").prop('selected', true);
-                // $("select[name='StartTime'] option").filter(function() {
-                //     return $(this).val() == msg.options['start'];
-                // }).prop('selected', true);
-                // $("select[name='StopTime'] option").filter(function() {
-                //     return $(this).val() == msg.options['end'];
-                // }).prop('selected', true);
                 $("input[name='submit']").click();
             }
+            
             if(filename == "Employee_FormatEntry1.asp"){
-                setTimeout(function(){ $("input[name='button2']").click(); }, setOpt(msg.options['delay'], 400));
+                if($("input[name='xbutton']").length > 0){
+                    $("input[name='xbutton']").click();
+                } else {
+                    console.log(setOpt(msg.options['delay']);
+                    setTimeout(function(){ $("input[name='button2']").click(); }, setOpt(msg.options['delay'], 10000));
+                }
             }
         }
     });
@@ -40,7 +26,6 @@ function setOpt(option, def) {
     if(option === null || option === "" || option === " "){
         return def;
     } else {
-        console.log("[" + option + "]");
         return option;
     }
 }
