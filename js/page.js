c1f7ff1bc0fb8d5e0ca5bcc8fa6e7e99e8627242
Joseph Larson <larson.joseph@gmail.com>
2013-10-03 05:54:49 -0600

    Fixed type, 0.4

--- js/page.js
+++ js/page.js
@@ -6,7 +6,6 @@ $(function(){
             var filename = window.location.href.substr( window.location.href.lastIndexOf("/") + 1 );
             
             if(filename == "Employee_FormatEntry1.asp?FormatID=96&EmpID=32957&Admin=&FormatName=Advance%20VTO"){
-                $("select[name='ExceptionDate'] :nth-child(3)").prop('selected', true);
                 $("input[name='submit']").click();
             }
             
@@ -14,8 +13,8 @@ $(function(){
                 if($("input[name='xbutton']").length > 0){
                     $("input[name='xbutton']").click();
                 } else {
-                    console.log(setOpt(msg.options['delay']);
-                    setTimeout(function(){ $("input[name='button2']").click(); }, setOpt(msg.options['delay'], 10000));
+                    console.log(setOpt(msg.options['delay']));
+                    setTimeout(function(){ $("input[name='button2']").click(); }, setOpt(msg.options['delay'], 0));
                 }
             }
         }
